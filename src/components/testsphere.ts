680fbc79d92d3b7bc6c08311d0374166ead7cd95
Ludovico Spitaleri <ludovico.spitaleri@gmail.com>
2023-02-14 14:18:20 +0100

    removed import

--- src/components/testsphere.ts
+++ src/components/testsphere.ts
@@ -1,6 +1,5 @@
 import { MeshBuilder } from "@babylonjs/core"
 import BaseComponent from "../core/base"
-import State from "../core/state"
 
 export default class Sphere extends BaseComponent {
     constructor() {
