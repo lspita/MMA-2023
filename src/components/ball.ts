14b692a8f6ec8c3f4291b696038176ce4d5d1e4b
spitaa <ludovico.spitaleri@gmail.com>
2023-02-14 20:46:01 +0100

    wisetree

--- src/components/ball.ts
+++ src/components/ball.ts
@@ -5,7 +5,7 @@ import State from "../core/state"
 export default class Ball extends BaseComponent {
     constructor(name: string) {
         super(name)
-        ModelImporter.fromObj(require("../../assets/models/golfball/golfball.obj"), this.mesh)
+        ModelImporter.fromObj(require("../../assets/models/wisetree/wisetree.obj"), this.mesh)
     }
     render(): void {
         State.scene.registerBeforeRender(() => {
