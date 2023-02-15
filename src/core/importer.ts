7a1e1e50040c4aaad5d0db197bd02c23aebf40be
Ludovico Spitaleri <ludovico.spitaleri@gmail.com>
2023-02-15 06:49:08 +0100

    removed scene

--- src/core/importer.ts
+++ src/core/importer.ts
@@ -1,10 +1,9 @@
 import { Mesh, SceneLoader } from "@babylonjs/core"
 import "@babylonjs/loaders"
-import State from "./state"
 
 export default class ModelImporter {
-    static async fromObj(model: string, parent: Mesh, scene = State.scene) {
-        let { meshes } = await SceneLoader.ImportMeshAsync("", "", model, scene)
+    static async fromObj(model: string, parent: Mesh) {
+        let { meshes } = await SceneLoader.ImportMeshAsync("", "", model)
         meshes.forEach(mesh => parent.addChild(mesh))
     }
 }
