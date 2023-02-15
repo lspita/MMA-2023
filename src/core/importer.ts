28ab6d23bc7738286c2055a5404d8ed79e35a0dd
Ludovico Spitaleri <ludovico.spitaleri@gmail.com>
2023-02-15 11:32:56 +0100

    working model importer

--- src/core/importer.ts
+++ src/core/importer.ts
@@ -1,9 +1,14 @@
-import { Mesh, SceneLoader } from "@babylonjs/core"
+import { Mesh, SceneLoader, Vector3 } from "@babylonjs/core"
 import "@babylonjs/loaders"
 
 export default class ModelImporter {
-    static async fromObj(model: string, parent: Mesh) {
-        let { meshes } = await SceneLoader.ImportMeshAsync("", "", model)
-        meshes.forEach(mesh => parent.addChild(mesh))
+    static fromObj(model: string, name: string) {
+        let parent = new Mesh(name)
+        SceneLoader.ImportMesh("", "", model, undefined, (meshes) => meshes.forEach((mesh) => {
+            parent.addChild(mesh)
+            mesh.name = mesh.name + "Model"
+            mesh.position = Vector3.Zero()
+        }))
+        return parent
     }
 }
