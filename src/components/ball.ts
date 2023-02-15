4cd0d3d8f52b33329e2201f2cd0f1583e79e46ca
Ludovico Spitaleri <ludovico.spitaleri@gmail.com>
2023-02-15 06:59:37 +0100

    golfball

--- src/components/ball.ts
+++ src/components/ball.ts
@@ -5,12 +5,6 @@ import State from "../core/state"
 export default class Ball extends BaseComponent {
     constructor(name: string) {
         super(name)
-        ModelImporter.fromObj(require("/assets/wisetree/wisetree.obj"), this.mesh)
-    }
-    render(): void {
-        State.scene.registerBeforeRender(() => {
-            this.mesh.rotation.x = State.deltaTime
-        })
-        super.render()
+        ModelImporter.fromObj(require("/assets/golfball/golfball.obj"), this.mesh)
     }
 }
