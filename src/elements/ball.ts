0a7e4a96a53485647e83524681aa6a1000594127
Ludovico Spitaleri <ludovico.spitaleri@gmail.com>
2023-02-16 16:31:47 +0100

    resized ball

--- src/elements/ball.ts
+++ src/elements/ball.ts
@@ -2,9 +2,9 @@ import { Color3, MeshBuilder, StandardMaterial } from "@babylonjs/core"
 import BaseElement from "../core/elements/base"
 
 export default class Ball extends BaseElement {
-    constructor(name: string) {
+    constructor(name: string, size = 0.75) {
         super(name)
-        this.mesh = MeshBuilder.CreateSphere(name, { diameter: 0.5 })
+        this.mesh = MeshBuilder.CreateSphere(name, { diameter: size })
         this.createMaterial(Ball.material, () => {
             Ball.material = new StandardMaterial("ballMat")
             Ball.material.diffuseColor = Color3.White()
