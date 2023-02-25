fa04bb41616ae104299f28833d120acaccd8dd02
spitaa <ludovico.spitaleri@gmail.com>
2023-02-25 16:32:24 +0100

    app as function

--- src/app.ts
+++ src/app.ts
@@ -7,126 +7,122 @@ import { CannonJSPlugin } from "@babylonjs/core"
 import createGrid from "./core/debug"
 import LevelGenerator from "./core/tileSystem/levelGenerator"
 import CANNON from "cannon"
-import Tile from "./core/tileSystem/tile"
 
-class App {
+function startGame() {
+    const gravityVector = new Vector3(0, -9.81, 0)
+    const physicsPlugin = new CannonJSPlugin(true, 10, CANNON)
 
-    constructor() {
+    // Create the canvas html element and attach it to the webpage
+    const canvas = document.createElement("canvas")
+    canvas.style.width = "100%"
+    canvas.style.height = "100%"
+    canvas.id = "gameCanvas"
+    document.body.appendChild(canvas)
 
-        const gravityVector = new Vector3(0, -9.81, 0)
-        const physicsPlugin = new CannonJSPlugin(true, 10, CANNON)
+    // Initialize babylon scene, engine and camera
+    State.engine = new Engine(canvas, true)
+    State.scene = new Scene(State.engine)
 
-        // Create the canvas html element and attach it to the webpage
-        const canvas = document.createElement("canvas")
-        canvas.style.width = "100%"
-        canvas.style.height = "100%"
-        canvas.id = "gameCanvas"
-        document.body.appendChild(canvas)
+    State.camera = new ArcRotateCamera(
+        'cam',
+        -Math.PI / 4, // Alpha
+        0.75, // Beta
+        50,
+        Vector3.Zero(),
+        State.scene, true
+    )
 
-        // Initialize babylon scene, engine and camera
-        State.engine = new Engine(canvas, true)
-        State.scene = new Scene(State.engine)
+    State.camera.upperRadiusLimit = State.camera.radius
+    State.camera.lowerRadiusLimit = State.camera.radius / 2
 
-        State.camera = new ArcRotateCamera(
-            'cam',
-            -Math.PI / 4, // Alpha
-            0.75, // Beta
-            50,
-            Vector3.Zero(),
-            State.scene, true
-        )
+    let lastKey: string
+    State.scene.onKeyboardObservable.add((kbInfo) => {
+        switch (kbInfo.type) {
+            case KeyboardEventTypes.KEYDOWN:
+                State.keys[kbInfo.event.key] = true
+                break
+            case KeyboardEventTypes.KEYUP:
+                State.keys[kbInfo.event.key] = false
+                break
+        }
 
-        State.camera.upperRadiusLimit = State.camera.radius
-        State.camera.lowerRadiusLimit = State.camera.radius / 2
+        lastKey = kbInfo.event.key
+    })
 
-        let lastKey: string
-        State.scene.onKeyboardObservable.add((kbInfo) => {
-            switch (kbInfo.type) {
-                case KeyboardEventTypes.KEYDOWN:
-                    State.keys[kbInfo.event.key] = true
-                    break
-                case KeyboardEventTypes.KEYUP:
-                    State.keys[kbInfo.event.key] = false
-                    break
+    const light = new DirectionalLight("light", new Vector3(0, 0, 1), State.scene)
+    const light2 = new DirectionalLight("light2", new Vector3(0, 0, -1), State.scene)
+    light.parent = State.camera
+    light2.parent = State.camera
+
+    // Hide/show the Inspector
+    window.addEventListener("keydown", (ev) => {
+        if (ev.key === 'i') {
+            if (State.scene.debugLayer.isVisible()) {
+                State.scene.debugLayer.hide()
+            } else {
+                State.scene.debugLayer.show()
             }
+        }
+    })
 
-            lastKey = kbInfo.event.key
-        })
+    window.addEventListener("resize", _ => {
+        State.engine.resize(true)
+    })
 
-        const light = new DirectionalLight("light", new Vector3(0, 0, 1), State.scene)
-        const light2 = new DirectionalLight("light2", new Vector3(0, 0, -1), State.scene)
-        light.parent = State.camera
-        light2.parent = State.camera
+    // Get delta time and time
+    let lastTime = 0
+    State.scene.registerBeforeRender(() => {
+        State.time = performance.now() * 0.001
+        State.deltaTime = State.time - lastTime
+        lastTime = State.time
+    })
 
-        // Hide/show the Inspector
-        window.addEventListener("keydown", (ev) => {
-            if (ev.key === 'i') {
-                if (State.scene.debugLayer.isVisible()) {
-                    State.scene.debugLayer.hide()
-                } else {
-                    State.scene.debugLayer.show()
-                }
-            }
-        })
+    // Camera movement
+    let cameraSpeedY: number = 0.0
+    let cameraSpeedX: number = 0.0
+    State.scene.registerBeforeRender(() => {
+        if (State.keys["ArrowDown"])
+            cameraSpeedY = Math.min(1, cameraSpeedY + 0.05)
+        else if (State.keys["ArrowUp"])
+            cameraSpeedY = Math.max(-1, cameraSpeedY - 0.05)
+        else cameraSpeedY = 0
 
-        window.addEventListener("resize", _ => {
-            State.engine.resize(true)
-        })
+        if (State.keys["ArrowRight"])
+            cameraSpeedX = Math.min(1, cameraSpeedX + 0.05)
+        else if (State.keys["ArrowLeft"])
+            cameraSpeedX = Math.max(-1, cameraSpeedX - 0.05)
+        else cameraSpeedX = 0
 
-        // Get delta time and time
-        let lastTime = 0
-        State.scene.registerBeforeRender(() => {
-            State.time = performance.now() * 0.001
-            State.deltaTime = State.time - lastTime
-            lastTime = State.time
-        })
+        State.camera.beta += cameraSpeedY * State.deltaTime
+        State.camera.beta = Scalar.Clamp(State.camera.beta, Math.PI / 8, Math.PI / 2.42)
 
-        // Camera movement
-        let cameraSpeedY: number = 0.0
-        let cameraSpeedX: number = 0.0
-        State.scene.registerBeforeRender(() => {
-            if (State.keys["ArrowDown"])
-                cameraSpeedY = Math.min(1, cameraSpeedY + 0.05)
-            else if (State.keys["ArrowUp"])
-                cameraSpeedY = Math.max(-1, cameraSpeedY - 0.05)
-            else cameraSpeedY = 0
+        State.camera.alpha += cameraSpeedX * 2 * State.deltaTime
+    })
 
-            if (State.keys["ArrowRight"])
-                cameraSpeedX = Math.min(1, cameraSpeedX + 0.05)
-            else if (State.keys["ArrowLeft"])
-                cameraSpeedX = Math.max(-1, cameraSpeedX - 0.05)
-            else cameraSpeedX = 0
-
-            State.camera.beta += cameraSpeedY * State.deltaTime
-            State.camera.beta = Scalar.Clamp(State.camera.beta, Math.PI / 8, Math.PI / 2.42)
-
-            State.camera.alpha += cameraSpeedX * 2 * State.deltaTime
-        })
-
-        const levelGenerator = new LevelGenerator(10, 30)
-        levelGenerator.createLevel()
+    const levelGenerator = new LevelGenerator(10, 30)
+    levelGenerator.createLevel()
 
 
-        const ball = new Ball("golfball")
-        ball.mesh.position.y = 1
+    const ball = new Ball("golfball")
+    ball.mesh.position.y = 1
 
-        //FISICA
-        State.scene.enablePhysics(gravityVector, physicsPlugin)
+    //FISICA
+    State.scene.enablePhysics(gravityVector, physicsPlugin)
 
-        ball.mesh.physicsImpostor = new PhysicsImpostor(ball.mesh, PhysicsImpostor.SphereImpostor, { mass: 1, restitution: 0.65 }, State.scene)
+    ball.mesh.physicsImpostor = new PhysicsImpostor(ball.mesh, PhysicsImpostor.SphereImpostor, { mass: 1, restitution: 0.65 }, State.scene)
 
 
 
 
-        createGrid(State.scene, 200)
-        State.scene.createDefaultEnvironment({
-            skyboxColor: Color3.Teal(),
-        })
-        canvas.focus()
-        State.engine.runRenderLoop(() => {
-            State.scene.render()
-        })
-    }
+    createGrid(State.scene, 200)
+    State.scene.createDefaultEnvironment({
+        skyboxColor: Color3.Teal(),
+    })
+    canvas.focus()
+    State.engine.runRenderLoop(() => {
+        State.scene.render()
+    })
 }
 
-new App()
+
+startGame()
