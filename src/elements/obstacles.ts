c13fba105139a55c001c104f34326ad4dcc07c4e
spitaa <ludovico.spitaleri@gmail.com>
2023-02-25 20:07:51 +0100

    removed test obstacle

--- src/elements/obstacles.ts
+++ src/elements/obstacles.ts
@@ -21,15 +21,3 @@ export function RotateObstacle(name: string, size: number) {
     })
     return pivot
 }
-
-export function TestObstacle(name: string, size: number) {
-    const box = MeshBuilder.CreateBox(name, { size: size })
-    const box2 = MeshBuilder.CreateBox(name, { height: 1, width: size + 1, depth: size + 1 })
-    box.addChild(box2)
-    box2.position.y = size / 2
-
-    State.scene.registerBeforeRender(() => {
-        box2.rotation.y += 5 * State.deltaTime
-    })
-    return box
-}
